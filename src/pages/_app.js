b4a95c1e7dab8338be98f140e370bf2ac8078ad4
unknown <fibsy79@yahoo.com>
2022-10-11 14:44:36 +0200

    added env

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }) {
     <>
     <Script
         strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-VMP80WQKMB`}
+        src={`https://www.googletagmanager.com/gtag/js?id='${process.env.GOOGLE_ANALYTICS}`}
       />
 
       <Script strategy="lazyOnload">
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }) {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-VMP80WQKMB', 
+            gtag('config', '${process.env.GOOGLE_ANALYTICS}', 
             );
                 `}
       </Script>
