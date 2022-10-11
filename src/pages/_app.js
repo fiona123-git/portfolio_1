6b641d995e4f891ed6a55fbf3b16283b1486ca50
unknown <fibsy79@yahoo.com>
2022-10-11 14:26:14 +0200

    made changes to _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,7 +4,21 @@ import Script from 'next/script'
 export default function App({ Component, pageProps }) {
   return (
     <>
-    
+    <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-VMP80WQKMB`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VMP80WQKMB', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
     
       <Theme>
         <Component {...pageProps} />
